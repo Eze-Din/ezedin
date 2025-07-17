6a52574c2d5546bdb50e0d1148ae0689d44be848
Eze Din <70455313+Eze-Din@users.noreply.github.com>
2025-07-17 11:56:22 +0300

    Update feedback

--- src/constants/index.js
+++ src/constants/index.js
@@ -186,6 +186,30 @@ export const feedback = [
     title: "Founder & Leader",
     img: people03,
   },
+  {
+    id: "feedback-1",
+    content:
+      "Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver.",
+    name: "Herman Jensen",
+    title: "Founder & Leader",
+    img: people01,
+  },
+  {
+    id: "feedback-2",
+    content:
+      "Money makes your life easier.",
+    name: "Steve Mark",
+    title: "Founder & Leader",
+    img: people02,
+  },
+  {
+    id: "feedback-3",
+    content:
+      "It is usually people in the money business, finance, and international trade that are really rich.",
+    name: "Kenn Gallagher",
+    title: "Founder & Leader",
+    img: people03,
+  },
 ];
 
 export const footerLinks = [
